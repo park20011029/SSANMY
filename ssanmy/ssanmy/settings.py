75b9a2ce3852d86d73d5cb1b8c782c80a4a67b5e
TRV449 <donghyun0215@naver.com>
2024-02-23 20:46:55 +0900

    Merge pull request #3 from teom142/main
    docs : 장고 버전 수정

--- ssanmy/ssanmy/settings.py
+++ ssanmy/ssanmy/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import my_settings
+from . import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'ssanmyApp',
 ]
 
 MIDDLEWARE = [
