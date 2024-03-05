1e8f55cbdaeb184ac6bc3ce2dae830eb21d30731
Kimd0ng <kdongh0406@gmail.com>
2024-03-05 14:53:58 +0900

    feat: main page 기초 추가

--- ssanmy/ssanmyApp/views.py
+++ ssanmy/ssanmyApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import testPost
+
+def index(request):
+    mods_list = testPost.objects.order_by('-test_title')
+    context = {'mods_list': mods_list}
+    return render(request, 'ssanmyApp/main_detail.html', context)
 
 # Create your views here.
