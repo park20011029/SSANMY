a52ed8dc8c51b9188c0779dd3b6956864dc4ce18
ryuse <87190298+RyuSeongho@users.noreply.github.com>
2024-03-11 21:38:42 +0900

    feat: 크롤링 사이트 추가

--- ssanmy/ssanmyApp/deal_crawler.py
+++ ssanmy/ssanmyApp/deal_crawler.py
@@ -3,6 +3,30 @@ def del_brace(str):
         if(str[i] == '['):
             return str[:i-1]
     return str
+
+def del_brace_quasar(str):
+    is_brac = 0
+    str2 = ""
+    for i in range(len(str)):
+        if(str[i] == ']'):
+            is_brac = 1
+            continue
+        if(is_brac == 1):
+            str2 += str[i]
+    if(is_brac == 0):
+        return str
+    return str2
+
+
+def is_unicord(str):
+    str2 = ""
+    for i in range(len(str)):
+        if ord(str[i]) >= 0 and ord(str[i]) <= 65535:
+            str2 += str[i]
+
+        str2 = del_brace_quasar(str2)
+    return str2
+
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ssanmy.settings")
 #import django
@@ -18,7 +42,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 scheduler = BackgroundScheduler()
 
-def find_obj():
+def find_fmkor():
     url = "https://www.fmkorea.com/index.php?mid=hotdeal&category=1254381811&page=1"
     response = requests.get(url)
 
@@ -30,11 +54,27 @@ def find_obj():
         data.append(del_brace(i.get_text().strip()))
     return data
 
+def find_quasar():
+    url = "https://quasarzone.com/bbs/qb_saleinfo"
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    title = soup.find_all('span', attrs={"class": "ellipsis-with-reply-cnt"})
+    data = []
+    for i in title:
+        data.append(is_unicord(i.get_text().strip()))
+    return data
+
 def job():
-    print('crawling data...')
-    obj = find_obj()
+    print('crawling data from fmkorea...')
+    obj = find_fmkor()
     for i in obj:
         testPost(test_title = i).save()
+    print('crawling data from quasar...')
+    obj2 = find_quasar()
+    for j in obj2:
+        testPost(test_title = j).save()
 
 def start_crawl():
     scheduler = BackgroundScheduler()
