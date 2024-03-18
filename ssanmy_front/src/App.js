01fe9949d076d339d24708a38137d46b2db1bd7f
Kimd0ng <kdongh0406@gmail.com>
2024-03-18 21:25:34 +0900

    Feat: model_deatil, footer routes 추가

--- ssanmy_front/src/App.js
+++ ssanmy_front/src/App.js
@@ -5,6 +5,8 @@ import {RecoilRoot} from 'recoil';
 import {BrowserRouter as Router, Routes, Route, Navigate, useLocation, BrowserRouter} from 'react-router-dom';
 import Main_detail from "./Components/Main_detail";
 import Login from "./Components/Login";
+import Footer from "./Components/Footer";
+import Model_detail from "./Components/Model_detail";
 
 function App() {
   return (
@@ -14,8 +16,10 @@ function App() {
               <Routes>
                   <Route path="/main" element={<Main_detail/>}/>
                   <Route path="/login" element={<Login/>}/>
+                  <Route path="/model" element={<Model_detail/>}/>
               </Routes>
           </BrowserRouter>
+      <Footer/>
       </>
   );
 }
