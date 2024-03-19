884dd33b28b850269a695ee7875cb9d3da054db8
park20011029 <rokmck156@gmail.com>
2024-03-19 12:26:27 +0900

    Feat: Add topbar, categori url mapping

--- ssanmy_front/src/App.js
+++ ssanmy_front/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
-import Topbar from "./Components/Topbar"
 import {RecoilRoot} from 'recoil';
 import {BrowserRouter as Router, Routes, Route, Navigate, useLocation, BrowserRouter} from 'react-router-dom';
 import Main_detail from "./Components/Main_detail";
 import Login from "./Components/Login";
+import Topbar from "./Pages/Topbar";
+import Categori from "./Pages/Categori";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
               <Routes>
                   <Route path="/main" element={<Main_detail/>}/>
                   <Route path="/login" element={<Login/>}/>
+                  <Route path="/categori" element={<Categori/>}/>
               </Routes>
           </BrowserRouter>
       </>
