8eb1186dd41e7f7fbe70f782b9097344cdf3855e
park20011029 <rokmck156@gmail.com>
2024-03-20 22:19:09 +0900

    Design: Categori 관련 ui 정렬부분과 상품 부분

--- ssanmy_front/src/Components/categori/Sort.jsx
+++ ssanmy_front/src/Components/categori/Sort.jsx
@@ -12,33 +12,48 @@ const CategoriTitle = styled.div`
 const Sorted = styled.div`
     display: flex;
 `;
+
 const BaseStyle = styled.div`
-    height: 3vh;
-    margin-left: 5vh;
-    border-radius: 20vh;
-    background: rgba(217, 217, 217, 0.50);
-    text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 4vh;
+      margin-left: 5vh;
+      border-radius: 20vh;
+      background: rgba(217, 217, 217, 0.50);
+      text-align: center;
+      font-weight: 600;
 `;
 
 const SortStyle1 = styled(BaseStyle)`
-    width: 5vw;
+    width: 4vw;
+`;
+
+const SortStyle3 = styled(SortStyle1)`
+    margin-left: 10vh;
 `;
 
 const SortStyle2 = styled(BaseStyle)`
-    width: 6vw;
+    width: 5vw;
+`;
+
+const SortHr = styled.hr`
+  background: rgba(0, 0, 0, 0.20);
+  width: 85vw;
+  margin: 2vh;
 `;
 
 function Sort() {
   return (
     <>
       <CategoriTitle>내가 선택한 카테고리</CategoriTitle>
-      <hr />
+      <SortHr />
       <Sorted>
-        <SortStyle1>최신</SortStyle1>
+        <SortStyle3>최신</SortStyle3>
         <SortStyle1>가격</SortStyle1>
         <SortStyle2>핫딜값</SortStyle2>
       </Sorted>
-      <hr />
+      <SortHr />
     </>
   );
 }
--- ssanmy_front/src/Pages/Categori.jsx
+++ ssanmy_front/src/Pages/Categori.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ModuleStyle from "../ModuleStyle.module.css";
 import Sort from "../Components/categori/Sort";
+import Inventory from "../Components/categori/Inventory";
 
 function Categori() {
     return(
     <div className={ModuleStyle.CategoriPage}>
         <Sort/>
+        <Inventory/>
     </div>
     )
   }
